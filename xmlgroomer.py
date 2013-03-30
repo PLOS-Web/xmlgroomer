028bbc991320d308864b60600af14060e93cd749
Vera Yin <verayin@gmail.com>
2013-03-29 23:02:55 -0700

    added tests

--- xmlgroomer.py
+++ xmlgroomer.py
@@ -5,8 +5,8 @@ import sys
 import re
 import lxml.etree as etree
 
-def fix_url():
-    for link in root.xpath("//ref//ext-link"):
+def fix_url(root):
+    for link in root.xpath("//ext-link"):
         h = '{http://www.w3.org/1999/xlink}href'
         assert h in link.attrib  # error: ext-link does not have href
         # remove whitespace
@@ -19,26 +19,29 @@ def fix_url():
             new_link = link.attrib[h].replace('http://', 'http://dx.doi.org/')
             print 'changing link from', link.attrib[h], 'to', new_link
             link.attrib[h] = new_link
+    return root
 
-def change_Clinical_Trial_to_Research_Article():
+def change_Clinical_Trial_to_Research_Article(root):
     for article_type in root.xpath("//article-categories//subj-group[@subj-group-type='heading']/subject"):
         if article_type.text == 'Clinical Trial':
             print 'changing article type from Clinical Trial to Research Article'
             article_type.text = 'Research Article'
+    return root
 
-def remove_period_after_comment_end_tag():
+def remove_period_after_comment_end_tag(root):
     for comment in root.xpath("//comment"):
         if comment.tail:
             print 'removing period after comment end tag'
             comment.tail = re.sub(r'^\.', r'', comment.tail)
+    return root
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
         e = etree.parse(sys.argv[1])
         root = e.getroot()
-        fix_url()
-        change_Clinical_Trial_to_Research_Article()
-        remove_period_after_comment_end_tag()
+        fix_url(root)
+        change_Clinical_Trial_to_Research_Article(root)
+        remove_period_after_comment_end_tag(root)
         e.write(sys.argv[2], xml_declaration = True, encoding = 'UTF-8')
         print 'done'
     else:
---
+++ xmlgroomertest.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# usage: nosetests xmlgroomertest.py
+
+import sys
+import re
+import lxml.etree as etree
+import xmlgroomer as x
+
+def test_fix_url():
+	before = etree.fromstring(\
+		'<article xmlns:xlink="http://www.w3.org/1999/xlink">\
+		<ext-link ext-link-type="uri" xlink:href="ht tp://10.1023/A:1020  830703012" xlink:type="simple">\
+		</ext-link>\
+		</article>')
+	after = etree.fromstring(\
+		'<article xmlns:xlink="http://www.w3.org/1999/xlink">\
+		<ext-link ext-link-type="uri" xlink:href="http://dx.doi.org/10.1023/A:1020830703012" xlink:type="simple">\
+		</ext-link>\
+		</article>')
+	assert etree.tostring(after) == etree.tostring(x.fix_url(before))
+
+def test_change_Clinical_Trial_to_Research_Article():
+	before = etree.fromstring(\
+		'<article>\
+		<article-categories>\
+		<subj-group subj-group-type="heading">\
+		<subject>Clinical Trial</subject>\
+		</subj-group>\
+		</article-categories>\
+		</article>')
+	after = etree.fromstring(\
+		'<article>\
+		<article-categories>\
+		<subj-group subj-group-type="heading">\
+		<subject>Research Article</subject>\
+		</subj-group>\
+		</article-categories>\
+		</article>')
+	assert etree.tostring(after) == etree.tostring(x.change_Clinical_Trial_to_Research_Article(before))
+
+def test_remove_period_after_comment_end_tag():
+	before = etree.fromstring('<article><comment></comment>.</article>')
+	after = etree.fromstring('<article><comment></comment></article>')
+	assert etree.tostring(after) == etree.tostring(x.remove_period_after_comment_end_tag(before))
