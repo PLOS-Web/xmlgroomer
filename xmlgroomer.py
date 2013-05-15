cae23795df3845fd9e4e62e8866f0909b30fc8cb
Vera Yin <verayin@gmail.com>
2013-05-14 19:02:41 -0700

    preserves empty titles in abstract

--- xmlgroomer.py
+++ xmlgroomer.py
@@ -191,7 +191,8 @@ def fix_empty_element(root):
     global output
     # starts from the leaves of the tree to remove nested empty elements
     for element in reversed(list(root.iterdescendants())):
-        if not element.text and not element.attrib and not element.getchildren():
+        if not element.text and not element.attrib and not element.getchildren() \
+            and not (element.tag == 'title' and 'abstract' in root.getroottree().getpath(element)):
             output += 'correction: removed empty element '+element.tag+' at '+root.getroottree().getpath(element)+'\n'
             element.getparent().remove(element)
     return root
